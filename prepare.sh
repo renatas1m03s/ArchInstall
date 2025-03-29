9b0362c7273c9c477642d539f080ad635bb9d51a
renatas1m03s <renata.s1m03s@gmail.com>
2025-03-29 10:30:16 -0300

    Added via command-line

--- prepare.sh
+++ prepare.sh
@@ -10,15 +10,18 @@
 
 echo -e "\n\n########################################################\n\n    Preparing system for ArchLinux\n\n########################################################\n\n"
 
+echo -e "\n############ Mounting RAM Drive ############\n"
 mkdir -v /root/ArchInstall
 mount -t tmpfs -o size=1024m ArchSetup /root/ArchInstall
 mount | grep ArchInstall
-sleep 5
+sleep 2
 
 pacman -Sy --noconfirm archlinux-keyring
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm git python-blessed p7zip
+
+echo -e "\n############ Downloading setup wizard files ############\n"
 
 git clone https://github.com/renatas1m03s/ArchInstall /root/ArchInstall
 
