1ffe7f2b4d66cace92571f2951aec686055f3ca1
renatas1m03s <renata.s1m03s@gmail.com>
2025-04-03 11:39:07 -0300

    Added via command-line

--- scripts/setpass.sh
+++ scripts/setpass.sh
@@ -5,3 +5,4 @@ echo $1 | passwd --stdin
 echo $1 | passwd $2 --stdin
 
 echo $2' ALL=(ALL) ALL' | sudo EDITOR='tee -a' visudo
+echo $2' ALL=(ALL) ALL NOPASSWD: /usr/bin/pacman' | sudo EDITOR='tee -a' visudo
