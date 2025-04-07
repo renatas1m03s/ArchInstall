b90741055bc01ed4d65ae57b8b6eedf488e85ba2
renatas1m03s <renata.s1m03s@gmail.com>
2025-04-07 12:11:40 -0300

    Added via command-line

---
+++ assets/functions/new-qemu-img.fish
@@ -0,0 +1,8 @@
+function new-qemu-img
+    if test (count $argv) = 2
+        qemu-img create -f qcom2 $argv[1] $argv[2]
+    else
+        printf "\n   Modo de usar: new-qemu-img nome_do_arquivo size(G)\n"
+        printf "\n   Ex.: new-qemu-img linuxtest.img 80G\n\n"
+    end
+end
