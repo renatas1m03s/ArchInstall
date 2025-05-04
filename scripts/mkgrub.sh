56b9e57f8133120219199b3c54be83a7fc71e85d
renatas1m03s <renata.s1m03s@gmail.com>
2025-05-03 23:41:33 -0300

    Added via command-line

--- scripts/mkgrub.sh
+++ scripts/mkgrub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i 's/loglevel=3 quiet/quiet loglevel=3 splash split_lock_detect=off/g' /etc/default/grub
+sed -i 's/#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/g' /etc/default/grub
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
