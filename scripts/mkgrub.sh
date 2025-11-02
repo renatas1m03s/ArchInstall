65cd913825fd6d1a0844b2aa5b8f0c810fe48ace
renatas1m03s <renata.s1m03s@gmail.com>
2025-11-02 12:16:00 -0300

    Added via command-line

--- scripts/mkgrub.sh
+++ scripts/mkgrub.sh
@@ -4,4 +4,5 @@ sed -i 's/loglevel=3 quiet/quiet loglevel=3 splash split_lock_detect=off/g' /etc
 sed -i 's/#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/g' /etc/default/grub
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+cd /home/$1/ArchInstall/assets/GrubTheme
+./install.sh
