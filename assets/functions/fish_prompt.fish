eb62e61caed0a1133216cc68fc5d1ef220cde3a3
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2026-02-15 11:07:30 -0300

    Add files via upload

--- assets/functions/fish_prompt.fish
+++ assets/functions/fish_prompt.fish
@@ -42,7 +42,7 @@ function fish_prompt
   ##
   ## Line 1
   ##
-  echo -n $hostcolor'╭─'$hotpink$current_user$white' at '$orange$__fish_prompt_hostname$white' in '$limegreen(pwd|sed "s=$HOME=⌁=")$turquoise
+  echo -n $hostcolor'╭─'$turquoise$current_user$turquoise'@'$turquoise$__fish_prompt_hostname$white':'$white(pwd|sed "s=$HOME=⌁=")$turquoise
   __fish_git_prompt " (%s)"
   echo
 
